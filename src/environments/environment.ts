8da7d502e567c55312fc6bb9ab5d3f845ca89c1f
Mamadou Saliou DIAWARA <linsan.saliou@gmail.com>
2024-08-04 12:10:54 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   secretKey: "",
   // urlAPi: "https://api-keycloak.security-mindset.fr",
+  
   urlAPi: "http://127.0.0.1:8445",
   apiKey: "linsan",
   keycloak: {
